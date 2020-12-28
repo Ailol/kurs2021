97764db24cd9e8c90c72bc77a1da5f1dfb8c88c0
ailol <ailonordsletta@gmail.com>
2020-12-28 16:35:26 +0100

    fixed repo

--- binder/.ipynb_checkpoints/spill1-checkpoint.py
+++
@@ -1,9 +0,0 @@
-from utils import hent_bruker_input
-
-
-gjett = 0   
-fasit = 7   
-while gjett != fasit:       
-    gjett = hent_bruker_input("Gjett et tall mellom 1 og 10: ")
-    
-print("Du gjettet riktig")
