92df484fe13d6c2a648ef03ed7bf30eecabd295a
ailol <ailonordsletta@gmail.com>
2020-12-28 16:33:00 +0100

    added updated notebooks

---
+++ matte.py
@@ -0,0 +1,34 @@
+# a)
+a = 2+3*4+6/2
+a1 = (2+3) * (4+6)/2 
+
+# b)
+b  = 1/2**3**2
+b1 = (1 / 2**2)**2
+
+
+# c)
+c = 1/(1+1/(1+2))
+c1 = 1/((1+1)/(1+2))
+
+# d)
+d = (1+2**3)**(5-3*2)
+d1 = (1+2**3)**((5-3)*2)
+
+# e)
+e = 2**(-1*3)
+e1 = 2 ** (-1*3)
+
+print("uendret oppgave: ")
+print("a:",a)
+print("b:",b)
+print("c:",c)
+print("d:",d)
+print("e:",e)
+
+print(" endret oppgave: ")
+print("a:",a1)
+print("b:",b1)
+print("c:",c1)
+print("d:",d1)
+print("e:",e1)
